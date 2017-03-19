c5cfbe27ce2417a46683be7b3b6def69c5c6df06
Daniel Redondo <daniel.redondo@gmail.com>
2017-03-19 19:17:26 +0100

    Login y logout

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -50,6 +50,27 @@ class AppController extends Controller
          */
         //$this->loadComponent('Security');
         //$this->loadComponent('Csrf');
+        
+        // Autorizacion
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'email',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+            'unauthorizedRedirect' => $this->referer() // If unauthorized, return them to page they were just on
+        ]);
+        
+        // Allow the display action so our pages controller
+        // continues to work.
+        $this->Auth->allow(['display']);
     }
 
     /**
--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -106,4 +106,38 @@ class UsersController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
+    
+    /**
+     * Login method
+     * 
+     * @return \Cake\Http\Response|NULL
+     */
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error('Your username or password is incorrect.');
+        }
+    }
+    
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Auth->allow(['logout']);
+    }
+    
+    /**
+     * Logout method
+     * 
+     * @return \Cake\Http\Response|NULL
+     */
+    public function logout()
+    {
+        $this->Flash->success('You are now logged out.');
+        return $this->redirect($this->Auth->logout());
+    }
 }
